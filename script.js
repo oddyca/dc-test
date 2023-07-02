2419c959cbed85e3b9667ad360194a74e4b1cc15
Siarhei Prakapovich <prokser27@icloud.com>
2023-07-03 01:40:00 +0300

    feat: add form validation and mock request

---
+++ script.js
@@ -0,0 +1,62 @@
+const form = document.querySelector('.form__inputs');
+const nameInput = document.getElementById('name-input');
+const emailInput = document.getElementById('email-input');
+const textInput = document.querySelector('.input__text');
+const nameError = document.getElementById('name-error');
+const emailError = document.getElementById('email-error');
+
+form.addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  nameError.innerText = '';
+  emailError.innerText = '';
+
+  // validation
+  if (nameInput.value.trim() === '') {
+    nameError.innerText = 'Введите ваше имя';
+    nameInput.classList.add('error');
+  } else {
+    nameInput.classList.remove('error');
+  }
+
+  if (emailInput.value.trim() === '') {
+    emailError.innerText = 'Введите вашу почту';
+    emailInput.classList.add('error');
+  } else {
+    emailInput.classList.remove('error');
+  }
+
+  if (!nameError.innerText && !emailError.innerText) {
+    await sendFormToServer();
+    form.reset();
+  }
+});
+
+// mock fetch request
+async function sendFormToServer() {
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    text: textInput.value,
+  };
+
+  try {
+    const response = await fetch('https://www.mockwebsite.com', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log('Form submitted successfully:', data);
+    } else {
+      console.error('Form submission failed:', response.status);
+    }
+  } catch (error) {
+    console.error('Form submission failed:', error);
+  }
+}
+
